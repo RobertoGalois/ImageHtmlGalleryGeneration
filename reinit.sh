3892bd17e0a71e6a1711299121944907a41b7bd9
RobertoGalois <victor.vague@live.fr>
2017-06-20 17:50:34 +0200

    Create reinit.sh

---
+++ reinit.sh
@@ -0,0 +1,12 @@
+############################
+# if you want to undo what #
+# galerie.sh did           #
+############################
+
+#!/bin/bash
+
+#delete all thumbnails
+rm ./thumb.*
+
+#delete galerie.html
+rm ./galerie.html
