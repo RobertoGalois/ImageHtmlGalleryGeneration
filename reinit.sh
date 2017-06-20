5ce5545ccb58b81dae27504e4ffcc932f87d7bab
RobertoGalois <victor.vague@live.fr>
2017-06-20 18:05:20 +0200

    Update reinit.sh

--- reinit.sh
+++ reinit.sh
@@ -1,12 +1,24 @@
 ############################
 # if you want to undo what #
 # galerie.sh did           #
+# it take in parameter the #
+# name of the html document#
+# if it is not specified,  #
+# it will delete           #
+# galerie.html             #
 ############################
 
 #!/bin/bash
 
+if [ $# -eq 1 ]
+then 
+	html_file_name=$1
+else
+	html_file_name='galerie'
+fi
+
 #delete all thumbnails
 rm ./thumb.*
 
-#delete galerie.html
-rm ./galerie.html
+#delete html file
+rm "./$html_file_name.html"
